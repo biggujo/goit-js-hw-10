83671196a05573c92408e1740c05b3eebd5d8fca
biggujo <gen.style22@gmail.com>
2023-07-12 15:48:15 +0300

    Move breeds fetch to cat-api.js

---
+++ src/js/cat-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] = 'live_GH5xb89M4mPrxepX0k4MzbvqZrhMGe5LLLaAglOFVXNzVie37XlZKLMyI7nBI51V';
+
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+
+export function fetchBreeds() {
+  return axios.get(BASE_URL)
+  .then((response) => {
+    if (response.status !== 200) {
+      throw new Error(String(response.status));
+    }
+
+    return response.data;
+  })
+  .then((breeds) => {
+    return breeds.map(({ name }) => name);
+  });
+}
