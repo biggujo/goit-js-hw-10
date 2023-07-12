923118b01c73ae5f8bc190748ea6721dd140075e
biggujo <gen.style22@gmail.com>
2023-07-12 16:04:20 +0300

    Add fetch cat image by cat id

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,10 +2,25 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] = 'live_GH5xb89M4mPrxepX0k4MzbvqZrhMGe5LLLaAglOFVXNzVie37XlZKLMyI7nBI51V';
 
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
-  return axios.get(BASE_URL)
+  return axios.get(`${BASE_URL}/breeds`)
+  .then((response) => {
+    if (response.status !== 200) {
+      throw new Error(String(response.status));
+    }
+
+    return response.data;
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  const parameters = new URLSearchParams({
+    breed_ids: breedId,
+  });
+
+  return axios.get(`${BASE_URL}/images/search?${parameters}`)
   .then((response) => {
     if (response.status !== 200) {
       throw new Error(String(response.status));
