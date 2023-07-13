c5bd7c74ff1d521542228c7e3097bd073bb0cb72
biggujo <gen.style22@gmail.com>
2023-07-13 22:53:02 +0300

    Add a slim-select <select>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import '~node_modules/modern-normalize/modern-normalize.css';
 import '~node_modules/slim-select/dist/slimselect.css';
 
+import SlimSelect from 'slim-select';
 import { fetchBreeds } from './js/cat-api';
+
+const select = new SlimSelect({
+  select: '#breed-select',
+});
 
 const refs = {
   selectBar: document.querySelector('.breed-select'),
