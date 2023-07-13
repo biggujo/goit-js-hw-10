7af821c682c7954e5225f63e9cd91639009ebb95
biggujo <gen.style22@gmail.com>
2023-07-14 00:20:42 +0300

    Add fetch and render markup

--- src/index.js
+++ src/index.js
@@ -2,31 +2,69 @@ import '~node_modules/modern-normalize/modern-normalize.css';
 import '~node_modules/slim-select/dist/slimselect.css';
 
 import SlimSelect from 'slim-select';
-import { fetchBreeds } from './js/cat-api';
-
-const select = new SlimSelect({
-  select: '#breed-select',
-});
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
 const refs = {
-  selectBar: document.querySelector('.breed-select'),
+  select: new SlimSelect({
+    select: '#breed-select',
+    events: {
+      afterChange: ([
+        {
+          value: breedId,
+        },
+      ]) => {
+        fetchCatByBreed(breedId)
+        .then((breedInfo) => {
+          renderCatInfoMarkup(createCatInfoMarkup(breedInfo));
+        });
+      },
+    },
+  }),
+  output: document.querySelector('.cat-info__info'),
 };
 
 fetchBreeds()
 .then((breedsArray) => {
-  renderOptionsMarkup(createOptionsMarkup(breedsArray));
+  setOptionsArray(createOptionsArray(breedsArray));
 })
 .catch(console.log);
 
-function createOptionsMarkup(breeds) {
+function createCatInfoMarkup([breedInfoById]) {
+  const {
+    url,
+    breeds,
+  } = breedInfoById;
+
+  const {
+    name,
+    description,
+    temperament,
+  } = breeds[0];
+
+  return `<div class='cat-info__column'>
+              <img src='${url}' alt='A photo of ${name} cat' width='400'>
+            </div>
+            <div class='cat-info__column'>
+              <h2 class='cat-info__title'>${name}</h2>
+              <p class='cat-info__text'>${description}</p>
+              <p><b>Temperament: </b>${temperament}</p>
+            </div>`;
+}
+
+function renderCatInfoMarkup(markup) {
+  refs.output.innerHTML = markup;
+}
+
+function createOptionsArray(breeds) {
   return breeds.map(({
     id,
     name,
-  }) => {
-    return `<option value='${id}'>${name}</option>`;
-  });
+  }) => ({
+    text: name,
+    value: id,
+  }));
 }
 
-function renderOptionsMarkup(markup) {
-  refs.selectBar.innerHTML = markup;
+function setOptionsArray(optionsArray) {
+  refs.select.setData(optionsArray);
 }
